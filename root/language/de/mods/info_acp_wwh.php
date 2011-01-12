4aa670247520ba98ad251ea62e030f53f3e065d9
Joas Schilling <nickvergessen@gmx.de>
2011-01-12 14:57:53 +0100

    Reword some stuff...

--- root/language/de/mods/info_acp_wwh.php
+++ root/language/de/mods/info_acp_wwh.php
@@ -35,9 +35,9 @@ $lang = array_merge($lang, array(
 	'WWH_DISP_GUESTS'			=> 'Gäste anzeigen',
 	'WWH_DISP_GUESTS_EXP'		=> 'Gäste im Zähler aufführen',
 	'WWH_DISP_HIDDEN'			=> 'Unsichtbare Benutzer anzeigen',
-	'WWH_DISP_HIDDEN_EXP'		=> 'Sollen unsichtbare Benutzer angezeigt werden? (Nur mit Berechtigung von phpBB3 selbst)',
+	'WWH_DISP_HIDDEN_EXP'		=> 'Sollen unsichtbare Benutzer angezeigt werden? (Berechtigung benötigt)',
 	'WWH_DISP_TIME'				=> 'Zeit anzeigen',
-	'WWH_DISP_TIME_FORMAT'		=> 'Zeit/Datums-Format',
+	'WWH_DISP_TIME_FORMAT'		=> 'Zeit-Format',
 	'WWH_DISP_HOVER'			=> 'als Hover-Effekt',
 	'WWH_DISP_TIME_EXP'			=> 'Entweder sehen alle Benutzer die Zeit oder niemand.',
 	'WWH_DISP_IP'				=> 'IP anzeigen',
@@ -49,13 +49,12 @@ $lang = array_merge($lang, array(
 	'WWH_RECORD_EXP'			=> 'Den Besucherrekord anzeigen und speichern.',
 	'WWH_RECORD_TIMESTAMP'		=> 'Datums-Format für den Rekord',
 	'WWH_RESET'					=> 'Besucherrekord zurücksetzen',
-	'WWH_RESET_1'				=> 'Besucherrekord zurücksetzen',
 	'WWH_RESET_EXP'				=> 'Setzt den Zeitpunkt und den Besucherrekord zurück.',
 	'WWH_RESET_TRUE'			=> 'Wenn du dieses Formular absendest,\nwird der Rekord zurückgesetzt.',// \n is the beginning of a new line
 									//no space after it
 
 	'WWH_SAVED_SETTINGS'		=> 'Einstellungen gespeichert.',
-	'WWH_SORT_BY'				=> 'sortiere Benutzer nach',
+	'WWH_SORT_BY'				=> 'Sortiere Benutzer nach',
 	'WWH_SORT_BY_EXP'			=> 'In welcher Reihenfolge sollen die Benutzer aufgelistet werden?',
 	'WWH_SORT_BY_0'				=> 'Benutzername A -> Z',
 	'WWH_SORT_BY_1'				=> 'Benutzername Z -> A',
@@ -70,7 +69,7 @@ $lang = array_merge($lang, array(
 	'WWH_VERSION_EXP'			=> 'Benutzer von heute anzeigen, oder aus dem Zeitraum der im nächsten Feld eingegeben wird',
 	'WWH_VERSION1'				=> 'Heute',
 	'WWH_VERSION2'				=> 'Zeitraum',
-	'WWH_VERSION2_EXP'			=> 'geben 0 ein, wenn die Besucher der letzten 24 Stunden angezeigt werden sollen',
+	'WWH_VERSION2_EXP'			=> 'Gib 0 ein, wenn die Besucher der letzten 24 Stunden angezeigt werden sollen',
 	'WWH_VERSION2_EXP2'			=> 'deaktiviert, wenn "Heute" ausgewählt wurde',
 	'WWH_VERSION2_EXP3'			=> 'Sekunden',
 
